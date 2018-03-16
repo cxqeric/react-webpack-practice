e745d3c2f75c2e678c1ba9f2c72c0726004fa533
KMKNKK <895025751@qq.com>
2018-03-16 23:32:00 +0800

    webpack init

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: KMKNKK 
+ * @Date: 2018-03-16 22:16:14 
+ * @Last Modified by: KMKNKK
+ * @Last Modified time: 2018-03-16 22:50:35
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+    <h1>Hello,world!</h1>,
+    document.getElementById('app')
+);
